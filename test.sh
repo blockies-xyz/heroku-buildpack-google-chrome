a9b47c31b3d13888579e3cb815155d336df13f91
SAR087 <68345593+SAR087@users.noreply.github.com>
2023-01-02 18:55:08 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+uname -a
 ZIP_LOCATION="/tmp/chromedriver.zip"
 curl -s -o $ZIP_LOCATION https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
 yes | apt-get install unzip wget gconf-service libappindicator1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo-gobject2 libdrm2 libgbm1 libgconf-2-4 libgtk-3-0 libnspr4 libnss3 libx11-xcb1 libxcb-dri3-0 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxi6 libxinerama1 libxrandr2 libxshmfence1 libxss1 libxtst6 fonts-liberation
@@ -6,4 +7,4 @@ yes | apt-get -y -d install --reinstall ./chrome.deb
 rm -rf./chrome.deb
 unzip -o $ZIP_LOCATION -d ./
 pwd
-rm -f $ZIP_LOCATION
+rm -rf $ZIP_LOCATION
