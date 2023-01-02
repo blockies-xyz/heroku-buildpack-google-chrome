b77a18e0cd61094aaccefa627f09f15b46fb9084
SAR087 <68345593+SAR087@users.noreply.github.com>
2023-01-02 20:58:27 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 uname -a
-ZIP_LOCATION="/tmp/chromedriver.zip"
-curl -s -o $ZIP_LOCATION https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+curl -s -o chromedriver.zip https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+unzip -o chromedriver.zip -d ./
+rm -rf chromedriver.zip
 curl -s -o chromedriver.deb https://raw.githubusercontent.com/blockies-xyz/heroku-buildpack-google-chrome/master/google-chrome-stable_current_amd64.deb
 yes | apt-get -y install --reinstall ./chromedriver.deb
 rm -rf chromedriver.deb
-unzip -o $ZIP_LOCATION -d ./
-rm -rf $ZIP_LOCATION
-whereis chromedriver
